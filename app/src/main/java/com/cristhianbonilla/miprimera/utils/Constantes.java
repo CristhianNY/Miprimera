95230f5af6aa956f47c92882da9948e6f7bce865
CristhianNY <cristhian4545@gmail.com>
2017-03-09 15:37:38 -0800

    Ultima version error pantalla home

---
+++ app/src/main/java/com/cristhianbonilla/miprimera/utils/Constantes.java
@@ -0,0 +1,11 @@
+package com.cristhianbonilla.miprimera.utils;
+
+/**
+ * Created by ASUS on 2/03/2017.
+ */
+
+public class Constantes {
+    public static final String FIREBASE_STORAGE_URL = "gs://miprimera2-41704.appspot.com/" ;
+    public static final String FIREBASE_STORAGE_IMAGES ="images";
+    public static final String FIREBASE_DATABASE_LOCATION_POST = "post" ;
+}
